007e399ae874ea587ea3937e2655dcac742b4420
zertmark <47692978+zertmark@users.noreply.github.com>
2020-06-03 23:22:59 +0300

    Add files via upload

--- pi_attack.py
+++ pi_attack.py
@@ -1,6 +1,16 @@
+#!/bin/python3 
+from pyroute2 import IPRoute
+import urllib
+from scapy import *
+from scapy.all import *
 import shutil
+import socket 
+from socket import *
 import netifaces 
+import scapy
+from os import system
 import configparser
+import threading
 import os
 from os import system
 def print_error(text):
@@ -17,25 +27,30 @@ def parse_config():
   interface=config["net_interface"]["net_interface"]
   startup=config["startup"]["run on startup"]
   self_dis=config['system_distraction']["add system-destroyer script if something goes wrong"]
+  snif=config["auto_scan"]["Enable auto-scan script"]
   check_script(self_dis)
   check_daemon(startup) 
-  enable_monitor_mode(interface)
-  exit()
+  scan_network(interface)
+  #enable_monitor_mode(interface)
  else:
   print_info("Config.ini wasn't found.Creating config...")
   interface=input("\nInterfaces:{}\nPlease enter your wifi interface:".format(netifaces.interfaces()))
   if interface in netifaces.interfaces():
     print_good("Setting {} as wifi interface".format(interface)) 
     auto_load=input("Do you want to start this script on startup?(Y/n):")
-    selfdis=input("Do you want to add script which destroys system(if something goes wrong)(y/N):")
+    selfdis=input("Do you want to add script which destroys system?(if something goes wrong)(y/N):")
+    scan=input("Do you want scan all network?(if your device will be connected to Internet. After scan you can check file map.html to view results)(Y/n):") 
     if auto_load.lower()!= "y" and auto_load.lower()!="n":
      print_error("Invalid choice.Setting script on startup as default")
      auto_load='Y'
     if selfdis.lower()!="y" and selfdis.lower()!="n":
      print_error("Invalid choice. Setting system-distroyer script off by default")
      selfdis='N'
+    if scan.lower()!="y" and scan.lower()!="n":
+     print_error("Invalid choice. Setting auto-scannig on by default")
+     scan='Y'
     print_info("Writing settings to config file")
-    write_config(config,interface,auto_load.upper(),selfdis.upper())
+    write_config(config,interface,auto_load.upper(),selfdis.upper(),scan)
     check_daemon(auto_load.upper())
     print_info("Please restart script to apply changes from config file")
     exit() 
@@ -52,10 +67,11 @@ def check_script(self_dis):
    print_good("System-destroyer script was created and ready to use(/bin/destroyer)(just type destroyer)")
  else:
   pass
-def write_config(config,interface,auto_load,selfdis): 
+def write_config(config,interface,auto_load,selfdis,scan): 
  config["net_interface"]={'Net_interface':interface}
  config["startup"]={'Run on startup':auto_load}
  config["system_distraction"]={'Add system-destroyer script if something goes wrong':selfdis}
+ config["auto_scan"]={"Enable auto-scan script":scan}
  with open("config.ini",'w') as config_file:
   config.write(config_file)
  print_good("Config was created")
@@ -99,4 +115,53 @@ def enable_monitor_mode(interface):
  else:
   print_error("Interface from config file wasn't found")    
   exit()
-main()
+#def sniff(snif,interface,filter):
+# if snif.lower() != 'n':
+#  if check_internet_connection != False:
+#   print_good("Starting sniffer...")
+#   sniffer=AsyncSniffer(iface=interface,filter=filter)
+#   sniffer.start()
+#   time.sleep(1)
+#   while len(sniffer.results)<3000:
+#    print(hashdump(sniffer.results)) 
+#   if len(sniffer.results) == 3000:
+#    sniffer.stop()
+#    wrpcap("{}.cap".format(generate_name()),pkts)
+#    print_good("Packets were saved. Restarting...")
+#    sniff(snif,interface,filter)
+#  else:
+#   print_error("There is no internet connection. Waiting 5 minutes to next try")
+#   time.sleep(300)
+#   sniff(snif,interface,filter)
+# else:
+#  pass
+def scan_network(interface):
+ if check_internet_connection != False:
+  if os.path.exists("nmap/"):
+   print_info("Starting to scan network")
+   system('cd nmap/ && ./nmapscan.pl')
+  else:
+   print_error("Directory nmap isn't found")
+   exit()
+ else:
+  print_error("Internet connection isn't establish")
+  exit() 
+#def mask(interface):
+# import socket
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.connect(("8.8.8.8", 80))
+# print(s.getsockname()[0])
+# ip = IPRoute()
+# info = [{'iface': x['index'],
+#         'addr': x.get_attr('IFA_ADDRESS'),
+#         'mask': x['prefixlen']} for x in ip.get_addr()]
+# print(info)
+# ip.close()
+def check_internet_connection():
+ try:
+  urllib.open("http://google.com")
+  return True
+ except IOError:
+  return False
+if __name__=="__main__":
+ main()
