a25cf0427ec42f188d7f3a86173a0a38420f794f
zertmark <47692978+zertmark@users.noreply.github.com>
2020-06-03 23:24:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt install hcxdumptool hcxtools
+apt install hcxdumptool hcxtools nmap traceroute iproute perl git wget  
 pip3 install configparser netifaces 
 chmod +x pi_attack.py
 echo "Installation is complete"
